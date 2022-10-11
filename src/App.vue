f1c49158a0765e0601524139b280ba453a746cf3
KittKatt88 <schmidt.helga88@gmail.com>
2022-10-11 16:41:32 -0500

    test branch start

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://picsum.photos/id/237/600/400" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://picsum.photos/id/238/600/400" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://picsum.photos/id/239/600/400" class="d-block w-100" alt="...">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
